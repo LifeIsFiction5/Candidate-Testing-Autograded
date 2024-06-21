2cfc7d819191eb28e8fd8279020aebfbf8dcbd16
LifeIsFiction5 <jacob_barrett@icloud.com>
2024-06-20 22:18:29 -0500

    Finished Task one for candidate testing.  Passed all six tests.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,35 +3,49 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride"
+let candidateAnswer = ""
 
 
 //TODO: Variables for Part 2
 let questions;
+
+"Make an array of Q here on an index and store each answer in an array"; //Questions within array will have index value, as will answers. 
+//comparing the two will be a function
 let correctAnswers;
 let candidateAnswers;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName = input.question("Please enter your first and last name.");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(`${question}`)
 
 }
+
+//!!input is an object.  Remember the '.' is an access modifier, and question is the method
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+  //think of Candidateanser and correct answer as a string.  They're a variable that is a string, so we may use string method on them.
 
+
+if (candidateAnswer.toLowerCase() == correctAnswer.toLowerCase()) {
+console.log("That's right!")
+} else {
+  console.log("Not quite!")
+}
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -40,9 +54,10 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
+  console.log("")
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log(`Hello, ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
