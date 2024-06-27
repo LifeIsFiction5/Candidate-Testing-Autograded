6d188277d67653ae9fa4308c67186f6913c085fa
LifeIsFiction5 <jacob_barrett@icloud.com>
2024-06-27 17:24:04 -0500

    Part one of graded assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,28 +7,48 @@ let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride"
-let candidateAnswer = ""
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
+let questions = [
+"Who was the first American woman in space?",
+"True or false: 5 kilometer == 5000 meters?",
+"(5 + 3)/2 * 10 = ?",
+"Given the array ['8', 'orbit', 'Trajectory', '45'], what entry is at index 2?",
+"What is the minimum crew size for the ISS?",
+];
 
 "Make an array of Q here on an index and store each answer in an array"; //Questions within array will have index value, as will answers. 
 //comparing the two will be a function
-let correctAnswers;
-let candidateAnswers;
+let correctAnswers = [
+"Sally Ride",
+"true",
+"40",
+"Trajectory",
+"3",
+];
+
+let candidateAnswers = [
+"",
+"",
+"",
+"",
+"",
+];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Please enter your first and last name.");
+candidateName = input.question("Please enter your first and last name: ");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(`${question}`)
+
+  candidateAnswer = input.question(`${question}`)
 
 }
 
@@ -38,7 +58,8 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  //think of Candidateanser and correct answer as a string.  They're a variable that is a string, so we may use string method on them.
+
+  //think of Candidateanwser and correctAnswer as a string.  They're a variable that is a string, so we may use string method on them.
 
 
 if (candidateAnswer.toLowerCase() == correctAnswer.toLowerCase()) {
